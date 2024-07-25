579d1d18dc385f6abea64c74412558c702bc822a
Heemank Verma <heemankv@gmail.com>
2024-07-25 22:01:58 +0530

    chore: correct variable name

--- crates/orchestrator/src/workers/data_submission.rs
+++ crates/orchestrator/src/workers/data_submission.rs
@@ -39,8 +39,8 @@ impl Worker for DataSubmissionWorker {
         let latest_proven_id: u64 = latest_proven_job_id.parse()?;
 
         // creating data submission jobs for latest blocks without pre-running data submission jobs jobs don't yet exist.
-        for x in latest_data_submission_id + 1..latest_proven_id + 1 {
-            create_job(JobType::DataSubmission, x.to_string(), HashMap::new()).await?;
+        for new_job_id in latest_data_submission_id + 1..latest_proven_id + 1 {
+            create_job(JobType::DataSubmission, new_job_id.to_string(), HashMap::new()).await?;
         }
 
         Ok(())
