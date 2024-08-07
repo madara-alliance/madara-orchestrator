7ebc83300dbcf2c1dfd9f5d92d9aea8c4640bcc3
Heemank Verma <heemankv@gmail.com>
2024-08-07 12:01:18 +0530

    update: fixed test cases

--- crates/orchestrator/src/tests/jobs/mod.rs
+++ crates/orchestrator/src/tests/jobs/mod.rs
@@ -2,7 +2,7 @@ use super::database::build_job_item;
 use crate::config::config;
 use crate::jobs::handle_job_failure;
 use rstest::rstest;
-
+use std::str::FromStr;
 #[cfg(test)]
 pub mod da_job;
 
@@ -505,8 +505,7 @@ fn build_job_item_by_type_and_status(job_type: JobType, job_status: JobStatus, i
     }
 }
 
-use std::str::FromStr;
-
+#[cfg(test)]
 impl FromStr for JobStatus {
     type Err = String;
 
@@ -517,6 +516,7 @@ impl FromStr for JobStatus {
             "PendingVerification" => Ok(JobStatus::PendingVerification),
             "Completed" => Ok(JobStatus::Completed),
             "VerificationTimeout" => Ok(JobStatus::VerificationTimeout),
+            "VerificationFailed" => Ok(JobStatus::VerificationFailed),
             "Failed" => Ok(JobStatus::Failed),
             s if s.starts_with("VerificationFailed(") && s.ends_with(')') => {
                 let reason = s[19..s.len() - 1].to_string();
@@ -527,6 +527,7 @@ impl FromStr for JobStatus {
     }
 }
 
+#[cfg(test)]
 impl FromStr for JobType {
     type Err = String;
 
@@ -543,21 +544,11 @@ impl FromStr for JobType {
 }
 
 #[rstest]
-#[case("DataSubmission", "Completed")] // code should panic here, how can completed move to dl queue ?
 #[case("SnosRun", "PendingVerification")]
-#[case("ProofCreation", "LockedForProcessing")]
-#[case("ProofRegistration", "Created")]
 #[case("DataSubmission", "Failed")]
-#[case("StateTransition", "Completed")]
-#[case("ProofCreation", "VerificationTimeout")]
-#[case("DataSubmission", "VerificationFailed()")]
 #[tokio::test]
-async fn test_handle_job_failure(#[case] job_type: JobType, #[case] job_status: JobStatus) -> color_eyre::Result<()> {
-    use color_eyre::eyre::eyre;
-
+async fn handle_job_failure_job_status_typical_works(#[case] job_type: JobType, #[case] job_status: JobStatus) {
     TestConfigBuilder::new().build().await;
-    dotenvy::from_filename("../.env.test")?;
-
     let internal_id = 1;
 
     let config = config().await;
@@ -567,7 +558,7 @@ async fn test_handle_job_failure(#[case] job_type: JobType, #[case] job_status: 
     let mut job = build_job_item(job_type.clone(), job_status.clone(), internal_id);
     let job_id = job.id;
 
-    // if testcase is for Failure, add last_job_status to job's metadata
+    // if test case is for Failure, add last_job_status to job's metadata
     if job_status == JobStatus::Failed {
         let mut metadata = job.metadata.clone();
         metadata.insert("last_job_status".to_string(), "VerificationTimeout".to_string());
@@ -583,7 +574,7 @@ async fn test_handle_job_failure(#[case] job_type: JobType, #[case] job_status: 
     match response {
         Ok(()) => {
             // check job in db
-            let job = config.database().get_job_by_id(job_id).await?;
+            let job = config.database().get_job_by_id(job_id).await.expect("Unable to fetch Job Data");
 
             if let Some(job_item) = job {
                 // check if job status is Failure
@@ -592,15 +583,45 @@ async fn test_handle_job_failure(#[case] job_type: JobType, #[case] job_status: 
                 assert_ne!(None, job_item.metadata.get("last_job_status"));
 
                 println!("Handle Job Failure for ID {} was handled successfully", job_id);
-            } else {
-                return Err(eyre!("Unable to fetch Job Data"));
             }
         }
         Err(err) => {
-            let expected = eyre!("Invalid state exists on DL queue: Completed");
+            panic!("Test case should have passed: {} ", err);
+        }
+    }
+}
+
+#[rstest]
+// code should panic here, how can completed move to dl queue ?
+#[case("DataSubmission", "Completed")]
+#[tokio::test]
+async fn handle_job_failure__job_status_completed_fails(#[case] job_type: JobType, #[case] job_status: JobStatus) {
+    use color_eyre::eyre::eyre;
+
+    TestConfigBuilder::new().build().await;
+    let internal_id = 1;
+
+    let config = config().await;
+    let database_client = config.database();
+
+    // create a job
+    let job = build_job_item(job_type.clone(), job_status.clone(), internal_id);
+    let job_id = job.id;
+
+    // feeding the job to DB
+    database_client.create_job(job.clone()).await.unwrap();
+
+    // calling handle_job_failure
+    let response = handle_job_failure(job_id).await;
+
+    match response {
+        Ok(()) => {
+            panic!("Test call to handle_job_failure should not have passed");
+        }
+        Err(err) => {
             // Should only fail for Completed case, anything else : raise error
+            let expected = eyre!("Invalid state exists on DL queue: {}", job_status);
             assert_eq!(err.to_string(), expected.to_string());
         }
     }
-    Ok(())
 }
