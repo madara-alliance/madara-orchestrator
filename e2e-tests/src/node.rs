ffd999f4492ba8d9c06f3bea89afe287ff3ce907
apoorvsadana <95699312+apoorvsadana@users.noreply.github.com>
2024-10-08 23:28:32 +0530

    remove release from e2e

--- e2e-tests/src/node.rs
+++ e2e-tests/src/node.rs
@@ -40,7 +40,6 @@ impl Orchestrator {
         let mut command = Command::new("cargo");
         command
             .arg("run")
-            .arg("--release")
             .arg("--bin")
             .arg("orchestrator")
             .arg("--features")
