208ffb8b9fc6767f529a6f8a2b49a73025e34e11
Heemank Verma <heemankv@gmail.com>
2024-11-04 13:04:22 +0530

    fix: indexing for get_jobs_without_successor

--- migrations/00000000000000-init.js
+++ migrations/00000000000000-init.js
@@ -8,6 +8,9 @@ module.exports = {
         { key: { job_type: 1, internal_id: -1 }, unique: true },
         { key: { job_type: 1, status: 1, internal_id: -1 } },
         { key: { status: 1 } },
+        // primarily for get_jobs_without_successor
+        { key: { job_type: 1, status: 1 } },
+        { key: { job_type: 1, internal_id: 1 } },
       ]);
   },
 
@@ -17,5 +20,8 @@ module.exports = {
     await db.collection("jobs").dropIndex("job_type_1_internal_id_-1");
     await db.collection("jobs").dropIndex("job_type_1_status_1_internal_id_-1");
     await db.collection("jobs").dropIndex("status_1");
+    await db.collection("jobs").dropIndex("job_type_1_status_1");
+    await db.collection("jobs").dropIndex("job_type_1_internal_id_1");
   },
 };
+  
