3bac79bca96f16dd006dc95cdfc38cca3a75965f
mohiiit <mohit18343@iiitd.ac.in>
2024-11-08 16:29:48 +0530

    fix: program hash fixed and fact of the block registered

--- e2e-tests/src/anvil.rs
+++ e2e-tests/src/anvil.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 use alloy::dyn_abi::SolType;
 use alloy::network::EthereumWallet;
-use alloy::primitives::{Address, Bytes, I256, U256};
+use alloy::primitives::{fixed_bytes, Address, Bytes, I256, U256};
 use alloy::providers::ProviderBuilder;
 use alloy::signers::local::PrivateKeySigner;
 use alloy::sol;
@@ -55,21 +55,25 @@ impl AnvilSetup {
         let provider = ProviderBuilder::new().with_recommended_fillers().wallet(wallet).on_http(self.rpc_url.clone());
 
         let starknet_core_contract_client = StarknetCoreContract::deploy(&provider).await.unwrap();
-        println!("📦 Deployed starknet_core_contract at address: {}", starknet_core_contract_client.address());
+        log::debug!("📦 Deployed starknet_core_contract at address: {}", starknet_core_contract_client.address());
         let verifier_client = GPSVerifier::deploy(&provider).await.unwrap();
 
         // This is the fact hash calculated from get_fact_info() or mongodb job metadata
         // for block 66645
-        // let fact_hash = fixed_bytes!("129324e742e7c1ce700f7a99cbc83b4959ede9dff22e1bbaa7bd95396c3a6240");
-        // let _ = verifier_client.setValid(fact_hash).send().await.expect("Failed to set fact as valid");
-        // let _is_fact_valid = verifier_client.isValid(fact_hash).call().await.unwrap()._0;
-        // assert!(_is_fact_valid, "Fact should be valid");
-        // log::debug!("Is fact valid? {:?}", _is_fact_valid);
+        let fact_hash = fixed_bytes!("129324e742e7c1ce700f7a99cbc83b4959ede9dff22e1bbaa7bd95396c3a6240");
+        let _ = verifier_client.setValid(fact_hash).send().await.expect("Failed to set fact as valid");
+        let _is_fact_valid = verifier_client.isValid(fact_hash).call().await.unwrap()._0;
+        assert!(_is_fact_valid, "Fact should be valid");
+        log::debug!("Is fact valid? {:?}", _is_fact_valid);
 
-        println!("📦 Deployed verifier at address: {}", verifier_client.address());
+        log::debug!("📦 Deployed verifier at address: {}", verifier_client.address());
 
         let init_data = InitializeData {
-            programHash: U256::from(0u64),
+            programHash: U256::from_str_radix(
+                "853638403225561750106379562222782223909906501242604214771127703946595519856",
+                10,
+            )
+            .unwrap(),
             aggregatorProgramHash: U256::from(0u64),
             verifier: *verifier_client.address(),
             configHash: U256::from_str_radix(
