712b6ed9ccbaa2c02fc6f52428ee08e4654af37a
mohiiit <mohit18343@iiitd.ac.in>
2024-11-19 23:37:12 +0530

    resolve: removed bucket name from require_all

--- crates/orchestrator/src/cli/storage/aws_s3.rs
+++ crates/orchestrator/src/cli/storage/aws_s3.rs
@@ -2,7 +2,7 @@ use clap::Args;
 
 /// Parameters used to config AWS S3.
 #[derive(Debug, Clone, Args)]
-#[group(requires_all = ["bucket_name"])]
+#[group()]
 pub struct AWSS3CliArgs {
     /// Use the AWS s3 client
     #[arg(long)]
