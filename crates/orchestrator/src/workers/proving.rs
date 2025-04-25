09364275bbda16f1dc3ebce3ffaf14325d7c97fc
Heemank Verma <heemankv@gmail.com>
2025-04-25 13:18:15 +0530

    new prooving logic

--- crates/orchestrator/src/workers/proving.rs
+++ crates/orchestrator/src/workers/proving.rs
@@ -23,9 +23,40 @@ impl Worker for ProvingWorker {
             .get_jobs_without_successor(JobType::SnosRun, JobStatus::Completed, JobType::ProofCreation)
             .await?;
 
+        tracing::info!(
+            "Found {} successful snos jobs without proving jobs",
+            successful_snos_jobs.len()
+        );
+
+        // get the max number of proof registration jobs that can be currently created because of max capacity.
+        let max_cap = utils::env_utils::get_env_var_or_default("MADARA_ORCHESTRATOR_MAX_PARALLEL_PROVING_JOBS", "8");
+
+        tracing::info!("Max capacity for proving jobs is {}", max_cap);
+
         tracing::debug!("Found {} successful SNOS jobs without proving jobs", successful_snos_jobs.len());
 
-        for job in successful_snos_jobs {
+
+        let job_type = JobType::ProofCreation;
+        let statuses = vec![JobStatus::Created, JobStatus::LockedForProcessing, JobStatus::PendingVerification, JobStatus::PendingRetry];
+
+        let current_jobs = config.database().get_jobs_by_type_and_statuses(job_type, statuses).await?;
+
+        let current_jobs_count = current_jobs.len();
+        tracing::info!("Current jobs count: {}", current_jobs_count);
+
+        let max_jobs = max_cap.parse::<usize>().unwrap_or(50);
+        let remaining_capacity = max_jobs.saturating_sub(current_jobs_count);
+
+        tracing::info!("max_jobs {}", max_jobs);
+
+        tracing::info!("Remaining capacity for proving jobs is {}", remaining_capacity);
+
+        // get the first remaining_capacity jobs from successful_snos_jobs
+        let remaining_jobs = successful_snos_jobs.into_iter().take(remaining_capacity);
+
+        tracing::info!("Creating jobs for {} proving jobs", remaining_jobs.len());
+
+        for job in remaining_jobs {
             tracing::debug!(job_id = %job.internal_id, "Creating proof creation job for SNOS job");
             match create_job(JobType::ProofCreation, job.internal_id.to_string(), job.metadata, config.clone()).await {
                 Ok(_) => tracing::info!(block_id = %job.internal_id, "Successfully created new proving job"),
